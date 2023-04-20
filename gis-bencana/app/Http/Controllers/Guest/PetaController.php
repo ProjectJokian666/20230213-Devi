df026638cc780a12179cb78cf9173e7f288eedd9
ProjectJokian666 <projectjokian666@gmail.com>
2023-04-20 15:23:40 +0700

    kurang pop up dan legenda

--- gis-bencana/app/Http/Controllers/Guest/PetaController.php
+++ gis-bencana/app/Http/Controllers/Guest/PetaController.php
@@ -6,14 +6,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Wilayah;
+use App\Models\DataBencanaPerWilayah;
 
 class PetaController extends Controller
 {
-    public function peta()
+    public function peta(Request $request)
     {   
         $data = [
             'wilayah'=>Wilayah::all(),
+            // 'data_bencana'=>DataBencanaPerWilayah::all(),
         ];
+        // dd($data,$request->cari_tahun);
         return view('guest.peta.index',compact('data'));
     }
 }
--- gis-bencana/resources/views/guest/peta/index.blade.php
+++ gis-bencana/resources/views/guest/peta/index.blade.php
@@ -8,7 +8,15 @@
 <section class="section dashboard">
 	<div class="row justify-content-center card">
 		<div class="col-12">
-			<div id="maps" style="height:500px;width: 100%;"></div>
+			<div id="maps" style="height:500px;">
+				<div id="state-legend" class="legend">
+					<h6>Keterangan</h6>
+					<div><span style="background-color: #000000"></span>Sangat Tinggi</div>
+					<div><span style="background-color: #FF0000"></span>Tinggi</div>
+					<div><span style="background-color: #FFFF00"></span>Medium</div>
+					<div><span style="background-color: #008000"></span>Rendah</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
@@ -22,7 +30,22 @@ $a = asset('geo-map/kota.json');
 <script src="{{asset('mapbox/mapbox-gl.js')}}"></script>
 <script>
 	// console.log("<?php echo $a; ?>");
+	var device;
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            // true for mobile device
+		device = [
+			[111.75913914489564, -8.669228758626588],
+			[112.45263414965126, -7.170129037865038]
+			];
+	} else {
+            // false for not mobile device
+		device = [
+			[111.52119039287481, -8.040812976926816],
+			[112.69129014605602, -7.586629911014967]
+			];
+	}
 	mapboxgl.accessToken = '{{env("MAPBOX_KEY")}}';
+
 	const map = new mapboxgl.Map(
 	{
         // container ID
@@ -33,52 +56,160 @@ $a = asset('geo-map/kota.json');
         // starting position
 		center: [112.03, -7.824],
         // starting zoom
-		zoom: 11.5
+		zoom: 11.5,
+		maxBounds: device,
 	});
 
-	map.addControl(new mapboxgl.NavigationControl());
-	map.addControl(new mapboxgl.FullscreenControl());
+	map.on('load', function () {
+		// Add an image to use as a custom marker
+		map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', function(error, image) {
+			if (error) {
+				throw error
+			}
+			map.addImage('custom-marker', image);
 
+			map.addSource('maine', {
+				'type': 'geojson',
+				'data': {
+					'type': 'FeatureCollection',
+					'features': [
 
-	map.on('load', () => {
-            // Add a data source containing GeoJSON data.
-		map.addSource('maine', {
-			'type': 'geojson',
-			'data': {
-				'type': 'FeatureCollection',
-				'features': [
-					<?php 
-					foreach ($data['wilayah'] as $wilayah) {
-						echo '{'.file_get_contents("Data_Wilayah/".$wilayah->file_wilayah).'},';
+						<?php 
+						$a = '{';
+						$b = ',';
+
+						use App\Models\BencanaPerWilayah;
+						use App\Models\DataBencanaPerWilayah;
+
+					foreach ($data['wilayah'] as $wilayah) { // mengambil data wilayah dan menampilkannya 
+						$id = $wilayah->id;
+						
+						$jumlah_bencana_per_wilayah = 0;
+
+						$data_bencana = BencanaPerWilayah::where('id_wilayah','=',$wilayah->id)->get(); // get data bencana per wilayah
+						if ($data_bencana!=null) {
+							foreach ($data_bencana as $db) {
+
+								$total_data_bencana = DataBencanaPerWilayah::where('id_bencana_per_wilayah','=',$db->id_bencana_per_wilayah)->first(); // get data per bencana
+								if ($total_data_bencana!=null) {
+									$jumlah_bencana_per_wilayah += $total_data_bencana->jumlah;
+								}
+
+							}
+						}
+						echo $a;
+						echo file_get_contents("Data_Wilayah/".$wilayah->file_wilayah); // mengambil file berdasarkan database dan ditampilkan
+						echo $b;
+						echo '"id":'.'"'.$id.'",';
+						echo '"properties":{"nama":'.'"'.$wilayah->nama_wilayah.'"'.',"wilayah":'.'"'.$wilayah->nama_wilayah.'"'.',"bencana":'.$jumlah_bencana_per_wilayah.'}},';
+
 					}
+					
 					?>
+					
 					]
-			}
-		});
+				}
+			});
 
-            // Add a new layer to visualize the polygon.
-		map.addLayer({
-			'id': 'maine',
-			'type': 'fill',
-                // reference the data source
-			'source': 'maine', 
-			'layout': {},
-			'paint': {
-                    // blue color fill
-				'fill-color': '#0080ff', 
-				'fill-opacity': 0.05
-			}
-		});
-            // Add a black outline around the polygon.
-		map.addLayer({
-			'id': 'outline',
-			'type': 'line',
-			'source': 'maine',
-			'layout': {},
-			'paint': {
-				'line-color': '#000',
-				'line-width': 0.1
-			}
+			map.addLayer({ // Add a new layer to visualize the polygon.
+				'id': 'isi',
+				'type': 'fill', // reference the data source
+				'source': 'maine', 
+				'layout': {},
+				'paint': { // blue color fill
+					'fill-color': [
+						"case",
+						['<=', ['get', "bencana"], 15], "#008000",
+						['<=', ['get', "bencana"], 30], "#FFFF00",
+						['<=', ['get', "bencana"], 45], "#FF0000",
+						['>', ['get', "bencana"], 60], "#000000",
+						'#123456'
+						],
+					'fill-opacity': [
+						'case',
+						['boolean', ['feature-state', 'hover'], false],
+						1,
+						0.5
+						]
+				}
+			});
+
+			map.addLayer({ // Add a black outline around the polygon.
+				'id': 'batas',
+				'type': 'line',
+				'source': 'maine',
+				'layout': {},
+				'paint': {
+					'line-width': 1,
+					'line-color': '#f0ffff'
+				}
+			});
+
+			map.addLayer({ // Add a layer showing the places.
+				'id': 'places',
+				'type': 'symbol',
+				'source': 'places',
+				'layout': {
+					'icon-image': 'custom-marker',
+					'icon-allow-overlap': true
+				}
+			});
+
+			var popup1 = new mapboxgl.Popup({
+				closeButton: false,
+				closeOnClick: false
+			});
+
+			map.on('mousemove', 'isi', function(e) {
+				if (e.features.length > 0) {
+					if (hoveredStateId1) {
+                                // Change the cursor style as a UI indicator.
+						map.getCanvas().style.cursor = 'pointer';
+
+                                // Single out the first found feature.
+						var feature1 = e.features[0];
+
+                                // Display a popup with the name of the county
+						popup1.setLngLat(e.lngLat)
+						.setHTML('<h5><p class="text-center"><strong>' + "Kecamatan " + feature1.properties.nama + '</strong><br>' + "Prevalensi Kasus " + feature1.properties.bencana + " % " +
+							'</p></h5>')
+						.addTo(map);
+						map.setFeatureState({
+							source: 'maine',
+							id: hoveredStateId1
+						}, {
+							hover: false
+						});
+					}
+					hoveredStateId1 = e.features[0].id;
+					map.setFeatureState({
+						source: 'maine',
+						id: hoveredStateId1
+					}, {
+						hover: true
+					});
+				}
+			});
+
+			map.on('mouseleave', 'isi', function() {
+				if (hoveredStateId1) {
+					map.getCanvas().style.cursor = '';
+					popup1.remove();
+					map.setFeatureState({
+						source: 'maine',
+						id: hoveredStateId1
+					}, {
+						hover: false
+					});
+				}
+				hoveredStateId1 = null;
+			});
+
+		}
+		);//z
+
+		map.on('idle', function() {
+			map.resize()
 		});
 	});
 </script>
