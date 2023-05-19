e76630d249765b509e54da92a494bb10eed7e43f
ProjectJokian666 <ProjectJokian666@gmail.com>
2023-05-19 20:24:08 +0700

    file preview clear

--- gis-bencana/resources/views/admin/wilayah/add.blade.php
+++ gis-bencana/resources/views/admin/wilayah/add.blade.php
@@ -58,15 +58,7 @@
 					</form>
 				</div>
 			</div>
-			<div class="card">
-				<div class="card-body">
-					<h5 class="card-title" id="urlfile">URL</h5>
-				</div>
-			</div>
-			<div class="card">
-				<div class="card-body">
-					<h5 class="card-title">Preview File</h5>
-				</div>
+			<div class="card" id="peta_preview">
 				<div id="maps" style="height:500px;"></div>
 			</div>
 		</div>
@@ -76,101 +68,5 @@
 
 @push('jss')
 <script src="{{asset('mapbox/mapbox-gl.js')}}"></script>
-<script type="text/javascript">
-	var data_peta ="";
-	show_peta();
-
-	$('#file_wilayah').change(function(){
-		previewData(this);
-	});
-
-	function previewData(input) {
-
-		const data_file = input.files[0];
-		const reader = new FileReader();
-
-		reader.onload = function(e) {
-			const fileContent = e.target.result;
-			data_peta = fileContent;
-			show_peta();
-			// console.log(data_peta);
-		};
-
-		reader.readAsText(data_file);
-
-	}
-
-	function show_peta(){
-
-		var device;
-		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-			device = [
-				[111.75913914489564, -8.669228758626588],
-				[112.45263414965126, -7.170129037865038]
-				];
-		} else {
-            // false for not mobile device
-			device = [
-				[111.52119039287481, -8.040812976926816],
-				[112.69129014605602, -7.586629911014967]
-				];
-		}
-		mapboxgl.accessToken = '{{env("MAPBOX_KEY")}}';
-
-		const map = new mapboxgl.Map(
-		{
-			container: 'maps',
-			style: 'mapbox://styles/mapbox/{{env("MAPBOX_STYLE")}}',
-			center: [112.03, -7.824],
-			zoom: 11.5,
-			maxBounds: device,
-		});
-
-		map.on('load', function () {
-			map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', function(error, image) {
-				if (error) {
-					throw error
-				}
-				map.addImage('custom-marker', image);
-				console.log('data_peta');
-				map.addSource('maine', {
-					'type': 'geojson',
-					'data': {
-						'type': 'FeatureCollection',
-						'features': [{data_peta}]
-					}
-				});
-				console.log('data_peta');
-
-				map.addLayer({ 
-					'id': 'isi',
-					'type': 'fill', 
-					'source': 'maine', 
-					'layout': {},
-					'paint': {
-						'fill-color': '#fc7600',
-						'fill-opacity': 0.8
-					}
-				});
-
-				map.addLayer({
-					'id': 'batas',
-					'type': 'line',
-					'source': 'maine',
-					'layout': {},
-					'paint': {
-						'line-width': 1,
-						'line-color': '#f0ffff'
-					}
-				});
-
-			});
-
-			map.on('idle', function() {
-				map.resize()
-			});
-		});
-	}
-
-</script>
+@include('admin.wilayah.maps')
 @endpush
---
+++ gis-bencana/resources/views/admin/wilayah/maps.blade.php
@@ -0,0 +1,121 @@
+<script type="text/javascript">
+	// $('#peta_preview').hide();
+	let data_peta ="";
+	// show_peta();
+
+	$('#file_wilayah').change(function(){
+		previewData(this);
+	});
+
+	function previewData(input) {
+
+		const data_file = input.files[0];
+		const reader = new FileReader();
+
+		reader.onload = function(e) {
+			const fileContent = e.target.result;
+			data_peta = fileContent
+			show_peta()
+		};
+		reader.readAsText(data_file);
+
+	}
+
+	mapboxgl.accessToken = '{{env("MAPBOX_KEY")}}';
+
+	const map = new mapboxgl.Map(
+	{
+		container: 'maps',
+		style: 'mapbox://styles/mapbox/{{env("MAPBOX_STYLE")}}',
+		center: [112.03, -7.824],
+		zoom: 11.5,
+	});
+	// console.log(map);
+
+	show_peta()
+
+	function show_peta(){
+		if (map.getSource('data_source')) {
+			// console.log('ada')
+			map.removeLayer('outline')
+			map.removeLayer('maine')
+			map.removeSource('data_source');
+			// if(map.removeSource('data_source')){
+			// 	console.log('hapus data source')
+			// }
+			// else{
+			// 	console.log('tidak hapus data source')
+			// }
+			data_peta = data_peta.replaceAll(' ','');
+			data_peta = data_peta.replaceAll('\n','');
+			data_peta = JSON.parse("{"+data_peta+"}")
+			// console.log(data_peta)
+
+			map.addSource('data_source',{
+				'type':'geojson',
+				'data':data_peta,
+			});
+
+			// var dataLayer = map.getSource('data_source');
+			// console.log(dataLayer);
+			map.addLayer({
+				'id': 'maine',
+				'type': 'fill',
+				'source': 'data_source',
+				'layout': {},
+				'paint': {
+					'fill-color': '#0080ff',
+					'fill-opacity': 0.5
+				}
+			});
+			map.addLayer({
+				'id': 'outline',
+				'type': 'line',
+				'source': 'data_source',
+				'layout': {},
+				'paint': {
+					'line-color': '#000',
+					'line-width': 3
+				}
+			});
+
+		}
+		else{
+			// console.log('tidak');
+			map.on('load',()=>{
+				map.addSource('data_source',{
+					'type':'geojson',
+					'data': {
+						'type': 'Feature',
+						'geometry': {
+							'type': 'Polygon',
+							'coordinates': []
+						}
+					}
+				});
+				map.addLayer({
+					'id': 'maine',
+					'type': 'fill',
+					'source': 'data_source',
+					'layout': {},
+					'paint': {
+						'fill-color': '#0080ff',
+						'fill-opacity': 0.5
+					}
+				});
+				map.addLayer({
+					'id': 'outline',
+					'type': 'line',
+					'source': 'data_source',
+					'layout': {},
+					'paint': {
+						'line-color': '#000',
+						'line-width': 3
+					}
+				});
+				// console.log(map.getSource('data_source'))
+			})
+		}
+	}
+
+</script>
